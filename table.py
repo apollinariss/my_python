02f6e403c0c643701acacb66056fab5fc5dbe1be
Polina Shevchenko <st055994@student.spbu.ru>
2019-02-17 16:09:37 +0300

    init commit

---
+++ table.py
@@ -0,0 +1,11 @@
+no=int(input('Введите номер химического элемента\n'))
+if no==3:
+    print('Li')
+elif no==25:
+    print('Mn')
+elif no==80:
+    print('Hg')
+elif no==17:
+    print('Cl')
+else:
+    print('Подумайте еще раз и введите номер химического элемента')
